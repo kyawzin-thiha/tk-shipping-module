c9ed6d8faa8d7eecec86c458341524794e875ce5
Lilly Ko <lillyxcko@gmail.com>
2024-02-13 10:01:51 -0800

    user auth screens

--- client/src/_auth/forms/SignupForm.tsx
+++ client/src/_auth/forms/SignupForm.tsx
@@ -2,8 +2,25 @@ import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Link } from 'react-router-dom';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
 
 const SignupForm = () => {
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+
+
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
@@ -12,7 +29,7 @@ const SignupForm = () => {
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">Welcome, please enter your account details.</p>
 
-        <form onSubmit={form.handleSubmit()} className="flex flex-col gap-5 w-full mt-4"> 
+        <form /*onSubmit={form.handleSubmit()}*/ className="flex flex-col gap-5 w-full mt-4"> 
           <FormField
             control={form.control}
             name="name"
--- client/src/_root/pages/ItemsHome.tsx
+++ client/src/_root/pages/ItemsHome.tsx
@@ -1,9 +1,13 @@
 import LeftMenu from '@/components/shared/LeftMenu'
+import SignupForm from '@/_auth/forms/SignupForm'
+
 
 const ItemsHome = () => {
   return (
     <div>
       <LeftMenu />
+      
+      
     </div>
   )
 }
