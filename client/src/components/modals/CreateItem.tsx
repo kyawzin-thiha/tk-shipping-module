382d9056f8a3349d9d7776db6bc64fb102a32925
Lilly Ko <lillyxcko@gmail.com>
2024-02-16 13:19:16 -0800

    address book check

--- client/src/components/modals/CreateItem.tsx
+++ client/src/components/modals/CreateItem.tsx
@@ -23,7 +23,7 @@ import {
     CommandItem,
   } from "@/components/ui/command"
   import { Checkbox } from "@/components/ui/checkbox"
-  
+
   const shipmethod = [
     {
       value: "courier",
@@ -68,7 +68,7 @@ function CreateItem({ onClose }) {
           </DialogDescription>
           <br/>
           <div className="flex items-center space-x-2">
-            <Checkbox id="addressbook" />
+            <Checkbox id="addressbook" className="custom-checkbox" />
             <label
               htmlFor="terms"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
