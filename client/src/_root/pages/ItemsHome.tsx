f77d78ba4330c7486fa1ad5ab010d002396dd699
Lilly Ko <lillyxcko@gmail.com>
2024-02-23 12:48:29 -0800

    CSV button

--- client/src/_root/pages/ItemsHome.tsx
+++ client/src/_root/pages/ItemsHome.tsx
@@ -2,6 +2,7 @@ import LeftMenu from '@/components/shared/LeftMenu';
 import ItemsTable from '@/components/shared/ItemsTable';
 import { Input } from "@/components/ui/input";
 import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 const ItemsHome = () => {
   const [inputValue, setInputValue] = useState('');
@@ -32,25 +33,30 @@ const ItemsHome = () => {
           <div className="header-form-container">
             <h2 className="h2-semibold">Items to Package</h2>
             <p className="text-gray-500 mt-2">
-              Import items from ShopVox by entering the Sale Order #: 
+              Import items from ShopVox or CSV below: 
             </p>
           </div>
           <div className="form-container">
-            <div className="form-input-row">
-              <div className="flex items-center">
-                <label htmlFor="item-name" className="block text-sm font-medium text-gray-700 mb-4 mr-3">SO #:</label>
-                <div className="col-span-1 input-style-modal">
-                  <Input
-                    id="item-name"
-                    type="text"
-                    value={inputValue}
-                    onChange={handleChange}
-                    onKeyDown={handleKeyDown}
-                  />
-                </div>
+            <div className="form-input-row flex items-center">
+              <label htmlFor="item-name" className="block text-sm font-medium text-gray-700 mb-4 mr-3">ShopVox SO #:</label>
+              <div className="col-span-1 input-style-modal">
+                <Input
+                  id="item-name"
+                  type="text"
+                  value={inputValue}
+                  onChange={handleChange}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+              <div className="col-span-2 mb-4 ml-auto mr-10"> 
+                <Button className="blue-button-custom">
+                  <img src="/assets/icons/cloud-arrow-up-solid.svg" alt="add" className="plus-icon" />
+                  Import Items from CSV
+                </Button>
               </div>
             </div>
           </div>
+          
         </div>    
         <ItemsTable />
       </div>
